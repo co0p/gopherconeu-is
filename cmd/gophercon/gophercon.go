3ec60461ed65b1007c2c797a2b05af26d5d367e5
Julian Godesa <julian.godesa@googlemail.com>
2018-05-31 10:23:49 +0000

    adds a router

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-
 	log.Printf("Service is starting ...")
-	http.HandleFunc("/home", homeHandler())
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	r := mux.NewRouter()
+	r.HandleFunc("/home", homeHandler()).Methods(http.MethodGet)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("failed to start server %v", err.Error())
 	}
 }
